46fc8e126b7355b2aae3ec4f1c9196cc59c958bd
Chloemlla <happyclovo@gmail.com>
2026-07-30 21:19:20 +0800

    fix(ci): normalize Dart package import casing

--- lib/common/widgets/context_menu/dyn_menu_helper.dart
+++ lib/common/widgets/context_menu/dyn_menu_helper.dart
@@ -1,4 +1,4 @@
-part of 'package:PiliPlus/pages/dynamics/widgets/content_panel.dart';
+part of 'package:pili_plus/pages/dynamics/widgets/content_panel.dart';
 
 Widget dynTextMenuBuilder(
   SelectableRegionState state,
