8d618628609e4c63ccffaae8d5757c43a8fa75a3
Chloemlla <happyclovo@gmail.com>
2026-08-01 14:37:26 +0800

    fix: enable android core library desugaring

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -27,6 +27,7 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
+        isCoreLibraryDesugaringEnabled = true
     }
 
     defaultConfig {
@@ -107,6 +108,8 @@ flutter {
 }
 
 dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.5")
+
     val lumenCrashVersion =
         providers.gradleProperty("lumenCrashVersion")
             .orElse(providers.environmentVariable("LUMEN_CRASH_VERSION"))
